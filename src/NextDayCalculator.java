90f6d7f02ebbff341da1cf3ccf4d405c8f8836ba
Tuan le <leminhtuan48198@gmail.com>
2022-08-16 14:09:46 +0700

    finish all case

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,11 +1,61 @@
 public class NextDayCalculator {
 
     public static final String CONCATATION = "/";
+    public static final int FIRSTOFMONTH = 1;
+    public static final int FIRSTOFYEAR = 1;
+    public static final int LASTOFYEAR = 12;
 
     public static String getNextDay(int day, int month, int year){
-        if(day==31){
-            return 1+CONCATATION+(month+1)+CONCATATION+year;
+        int lastOfMonth = getLastOfMonth( month,year);
+        if(day== lastOfMonth){
+            if(month== LASTOFYEAR){
+                return FIRSTOFMONTH+CONCATATION+ FIRSTOFYEAR +CONCATATION+ ++year;
+            }
+            return FIRSTOFMONTH +CONCATATION+(month+ 1)+CONCATATION+year;
         }
-    return (day+1)+ CONCATATION +month+ CONCATATION +year;
+    return (day+ 1)+ CONCATATION +month+ CONCATATION +year;
 }
-}
+
+    public static int getLastOfMonth(int month,int year) {
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            case 2:
+                if( isLeapYear(year))
+                {return 29;}
+                    return 28;
+            default:
+                return 31;
+        }
+    }
+
+    private static boolean isLeapYear(int year) {
+        boolean checkIsLeapYear = false;
+
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if(isDivisibleBy4){
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if(isDivisibleBy100){
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if(isDivisibleBy400){
+                    checkIsLeapYear = true;
+                }
+            } else {
+                checkIsLeapYear = true;
+            }
+        }
+        return  checkIsLeapYear;
+    }
+    }
+
--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -24,4 +24,44 @@ public class NextDayCalculatorTest {
         String result=NextDayCalculator.getNextDay(day,month,year);
         assertEquals(expected,result);
     }
+    @Test
+    @DisplayName("test 30/4/2018")
+    void test30042018(){
+        int day=30;
+        int month=4;
+        int year=2018;
+        String expected="1/5/2018";
+        String result=NextDayCalculator.getNextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+    @Test
+    @DisplayName("test 28/2/2018")
+    void test28022018(){
+        int day=28;
+        int month=2;
+        int year=2018;
+        String expected="1/3/2018";
+        String result=NextDayCalculator.getNextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+    @Test
+    @DisplayName("test 29/2/2020")
+    void test29022020(){
+        int day=29;
+        int month=2;
+        int year=2020;
+        String expected="1/3/2020";
+        String result=NextDayCalculator.getNextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+    @Test
+    @DisplayName("test 31/12/2018")
+    void test31122018(){
+        int day=31;
+        int month=12;
+        int year=2018;
+        String expected="1/1/2019";
+        String result=NextDayCalculator.getNextDay(day,month,year);
+        assertEquals(expected,result);
+    }
 }
