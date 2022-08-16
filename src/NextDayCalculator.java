fbdefd2df6432b30a73029524e9f72b66397b35c
Tuan le <leminhtuan48198@gmail.com>
2022-08-16 11:26:44 +0700

    test case 31/1/2018

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,5 +1,11 @@
 public class NextDayCalculator {
-public static String getNextDay(int day,int month,int year){
-    return "2/1/2018";
+
+    public static final String CONCATATION = "/";
+
+    public static String getNextDay(int day, int month, int year){
+        if(day==31){
+            return 1+CONCATATION+(month+1)+CONCATATION+year;
+        }
+    return (day+1)+ CONCATATION +month+ CONCATATION +year;
 }
 }
--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -5,12 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NextDayCalculatorTest {
     @Test
-    @DisplayName("test 2/1/2018")
+    @DisplayName("test 1/1/2018")
     void test01012018(){
         int day=01;
         int month=01;
         int year=2018;
         String expected="2/1/2018";
+        String result=NextDayCalculator.getNextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+    @Test
+    @DisplayName("test 31/1/2018")
+    void test31012018(){
+        int day=31;
+        int month=01;
+        int year=2018;
+        String expected="1/2/2018";
         String result=NextDayCalculator.getNextDay(day,month,year);
         assertEquals(expected,result);
     }
